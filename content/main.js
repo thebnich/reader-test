c3a48dace4701f93dd55a7bb162e59f739dca687
Brian Nicholson <bnicholson@mozilla.com>
2012-08-14 17:47:16 -0400

    update test to only do parses

--- content/main.js
+++ content/main.js
@@ -8,7 +8,8 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 // Lazily-loaded browser scripts:
 [
-  ["UserAgent", "chrome://readability/content/UserAgent.js"]
+  ["UserAgent", "chrome://readability/content/UserAgent.js"],
+  ["Readability", "chrome://readability/content/Readability.js"]
 ].forEach(function (aScript) {
   let [name, script] = aScript;
   XPCOMUtils.defineLazyGetter(window, name, function() {
@@ -22,7 +23,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 let ua = new UserAgent("Mozilla/5.0 (Android; Mobile; rv:15.0) Gecko/15.0 Firefox/15.0");
 ua.override();
 
-// A full-featured browser, equivalent to what's used by the user.
 var Browser = function () {
   const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
   let browser = document.createElementNS(XUL_NS, "browser");
@@ -31,25 +31,14 @@ var Browser = function () {
   this._browser = browser;
 };
 
-// A stripped down browser with JS, images, etc. removed. This is what we're
-// currently using to parse readability in Fennec.
-var StrippedBrowser = function () {
-  let browser = document.createElement("browser");
-  browser.setAttribute("type", "content");
-
-  document.documentElement.appendChild(browser);
-  browser.webNavigation.allowAuth = false;
-  browser.webNavigation.allowImages = false;
-  browser.webNavigation.allowJavascript = false;
-  browser.webNavigation.allowMetaRedirects = true;
-  browser.webNavigation.allowPlugins = false;
-  this._browser = browser;
-};
-
-Browser.prototype = StrippedBrowser.prototype = {
+Browser.prototype = {
   onLoad: function (callback) {
     this._browser.addEventListener("DOMContentLoaded", function (evt) {
       let doc = evt.originalTarget;
+
+      if (doc.location == "about:blank")
+        return;
+
       // ignore on frames
       if (doc.defaultView != this._browser.contentWindow)
         return;
@@ -84,12 +73,7 @@ Driver.prototype = {
     xhr.send();
   },
 
-  _parseInWorker: function Reader_parseInWorker(uri, doc, callback) {
-    let worker = new ChromeWorker("readerWorker.js");
-    worker.onmessage = function (evt) {
-      callback(evt.data);
-    };
-
+  getReaderURI: function (uri) {
     let pathBase;
     try {
       pathBase = Services.io.newURI(".", null, uri).spec
@@ -97,26 +81,28 @@ Driver.prototype = {
       dump("Reader: could not get pathBase: " + e);
     }
 
+    return {
+      spec: uri.spec,
+      host: uri.host,
+      prePath: uri.prePath,
+      scheme: uri.scheme,
+      pathBase: pathBase
+    };
+  },
+
+  _parseInWorker: function Reader_parseInWorker(uri, doc, callback) {
+    let worker = new ChromeWorker("readerWorker.js");
+    worker.onmessage = function (evt) {
+      callback(evt.data);
+    };
+
     worker.postMessage({
-      uri: {
-        spec: uri.spec,
-        host: uri.host,
-        prePath: uri.prePath,
-        scheme: uri.scheme,
-        pathBase: pathBase
-      },
+      uri: this.getReaderURI(uri),
       doc: new XMLSerializer().serializeToString(doc)
     });
   },
 
-  checkReadability: function (url, td1, td2, td3, td4) {
-    function updateCheckColor() {
-      if (td1.textContent && td3.textContent) {
-        let bg = (td1.textContent == td3.textContent) ? "#4b6" : "b64";
-        td3.style.backgroundColor = bg;
-      }
-    }
-    
+  checkReadability: function (url, td1, td2) {
     let fullBrowser = new Browser();
     let uri = Services.io.newURI(url, null, null);
 
@@ -136,26 +122,11 @@ Driver.prototype = {
             body.insertBefore(title, body.firstChild);
           };
         }
-        updateCheckColor();
         fullBrowser.remove();
       });
     }.bind(this));
 
-    let strippedBrowser = new StrippedBrowser();
-    strippedBrowser.onLoad(function (doc) {
-      return;
-      let start = Date.now();
-      let readability = getReadability(doc);
-      readability.check(function (result) {
-        td4.textContent = (Date.now() - start);
-        td3.textContent = result;
-        updateCheckColor();
-        strippedBrowser.remove();
-      });
-    });
-
     fullBrowser.loadURI(url);
-    strippedBrowser.loadURI(url);
   }
 };
 
@@ -177,12 +148,8 @@ window.addEventListener("load", function () {
       tr.appendChild(td1);
       let td2 = frame.contentDocument.createElement("td");
       tr.appendChild(td2);
-      let td3 = frame.contentDocument.createElement("td");
-      tr.appendChild(td3);
-      let td4 = frame.contentDocument.createElement("td");
-      tr.appendChild(td4);
 
-      driver.checkReadability(url, td1, td2, td3, td4);
+      driver.checkReadability(url, td1, td2);
     }
   });
 }, false);
